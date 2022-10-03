5daff1ba54d3ef2755494bbfeb7cdeb0ce989b4c
AndreiaJM <andreia.ajms@gmail.com>
2022-10-03 18:20:58 -0300

    Api para calculo do teorema de pitagoras

--- app.py
+++ app.py
@@ -1,17 +1,42 @@
-from flask import Flask
-import os
+import math
+from flask import Flask,jsonify,request
+import json
 
-app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def Olamundo():
-    return 'Tetando a primeira rota, insira teste na url'
+app=Flask(__name__)
 
-@app.route('/teste', methods=['GET'])
-def teste():
-    return 'Tetando a segunda rota'
+@app.route('/',methods=['GET'])
+def ola_mundo():
+    return {"Ola":"Ola mundo"}
+
+
+@app.route('/calcular', methods=['POST'])
+def postCalcular():
+
+    body = json.loads(request.data)
+
+    hpt=float(body['hipotenusa'])
+    co=float(body['catetooposto'])
+    ca=float(body['catetoadjacente'])
+
+    if ca>co and hpt==0:
+        res = pow(ca,2)+pow(co,2)
+        body["hipotenusa"]= res
+    elif hpt>ca and co==0:
+        res = pow(hpt,2)-pow(ca,2)
+        body["catetooposto"]= res
+    elif hpt>co and ca==0:
+        res = pow(hpt,2)-pow(co,2)
+        body["catetoadjacente"]= res
+    else:
+        res='Verifique os dados e tente novamente'
+
+    body["resultado"]=res
+
+    return body
+
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
 
 
